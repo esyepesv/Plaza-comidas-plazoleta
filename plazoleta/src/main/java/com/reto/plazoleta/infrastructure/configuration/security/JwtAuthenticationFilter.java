56270c7cde650d31c953b78a4dcb47e5481c0360
esyepesv <stiven.yepes@pragma.com.co>
2023-08-17 21:31:18 -0500

    cambios en el acceso spring security

--- plazoleta/src/main/java/com/reto/plazoleta/infrastructure/configuration/security/JwtAuthenticationFilter.java
+++ plazoleta/src/main/java/com/reto/plazoleta/infrastructure/configuration/security/JwtAuthenticationFilter.java
@@ -1,14 +1,12 @@
 package com.reto.plazoleta.infrastructure.configuration.security;
 
-import com.reto.plazoleta.application.auth.JwtService;
 //import com.users.users.domain.model.Role;
 //import com.users.users.infrastructure.output.jpa.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+        import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
